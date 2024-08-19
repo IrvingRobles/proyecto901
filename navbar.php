314b88b0610d91d81e97d7e32e4cc7f4cd496781
IrvingRobles <dsm21190235.irobles@alumnos.utsv.edu.mx>
2024-08-19 17:21:10 -0600

    alexis

--- navbar.php
+++ navbar.php
@@ -37,7 +37,7 @@
                             <a class="nav-link" href="kevin.php">Kevin Ramirez</a>
                         </div>
                         <div class="col-xs-6 col-sm-6 col-md-6 col-lg-6 col-xl-6 nav-item">
-                            <a class="nav-link" href="kevin.php">Alexis torres</a>
+                            <a class="nav-link" href="alexis.php">Alexis torres</a>
                         </div>
                     </div> 
                 </div>    
